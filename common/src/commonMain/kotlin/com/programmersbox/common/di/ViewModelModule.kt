9ef8ab8c754ea411e9fa7cff306f2623c8b14191
jacobrein <jacob.rein@gabb.com>
2026-01-21 07:08:09 -0700

    Refactor: Simplify ViewModel declarations in Koin module
    This commit refactors the Koin `viewModelModule` to simplify the declaration of several ViewModels.
    
    The manual `viewModel { ... }` factory definitions for `CivitAiDetailViewModel`, `CivitAiModelImagesViewModel`, `CivitAiUserViewModel`, and `ListDetailViewModel` have been replaced with the more concise `viewModelOf()` constructor DSL.
    
    This change improves readability and maintainability of the dependency injection setup without altering runtime behavior.

--- common/src/commonMain/kotlin/com/programmersbox/common/di/ViewModelModule.kt
+++ common/src/commonMain/kotlin/com/programmersbox/common/di/ViewModelModule.kt
@@ -12,41 +12,20 @@ import com.programmersbox.common.presentation.images.CivitAiImagesViewModel
 import com.programmersbox.common.presentation.lists.ListDetailViewModel
 import com.programmersbox.common.presentation.lists.ListViewModel
 import com.programmersbox.common.presentation.qrcode.QrCodeScannerViewModel
-import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 fun viewModelModule() = module {
     viewModelOf(::CivitAiViewModel)
     viewModelOf(::CivitAiSearchViewModel)
-    viewModel { CivitAiDetailViewModel(get(), it.get(), get()) }
-    viewModel {
-        CivitAiModelImagesViewModel(
-            modelId = it.get(),
-            dataStore = get(),
-            network = get(),
-            database = get()
-        )
-    }
-    viewModel {
-        CivitAiUserViewModel(
-            network = get(),
-            dataStore = get(),
-            database = get(),
-            username = it.get()
-        )
-    }
     viewModelOf(::FavoritesViewModel)
     viewModelOf(::QrCodeScannerViewModel)
     viewModelOf(::CivitAiImagesViewModel)
     viewModelOf(::ListViewModel)
-    viewModel {
-        ListDetailViewModel(
-            listDao = get(),
-            uuid = it.get()
-        )
-    }
-
     viewModelOf(::BackupViewModel)
     viewModelOf(::RestoreViewModel)
+    viewModelOf(::ListDetailViewModel)
+    viewModelOf(::CivitAiDetailViewModel)
+    viewModelOf(::CivitAiUserViewModel)
+    viewModelOf(::CivitAiModelImagesViewModel)
 }

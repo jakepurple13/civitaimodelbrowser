0c1764b84c969b9e2cc07d260fd1fd5cc51b03fe
jacobrein <jacob.rein@gabb.com>
2026-02-25 11:42:17 -0700

    - better handling of platform related settings

--- common/src/androidMain/kotlin/com/programmersbox/common/di/NavigationModule.android.kt
+++ common/src/androidMain/kotlin/com/programmersbox/common/di/NavigationModule.android.kt
@@ -2,10 +2,9 @@ package com.programmersbox.common.di
 
 import com.programmersbox.common.presentation.settings.AndroidSettingsScreen
 import org.koin.core.module.Module
-import org.koin.dsl.navigation3.navigation
 
 actual fun Module.platformSettingsNavigation() {
-    navigation<AndroidSettingsScreen> {
+    settingsNavigation<AndroidSettingsScreen> {
         AndroidSettingsScreen()
     }
 }
--- common/src/jvmMain/kotlin/com/programmersbox/common/di/NavigationModule.jvm.kt
+++ common/src/jvmMain/kotlin/com/programmersbox/common/di/NavigationModule.jvm.kt
@@ -2,8 +2,7 @@ package com.programmersbox.common.di
 
 import com.programmersbox.common.presentation.settings.JvmSettingsScreen
 import org.koin.core.module.Module
-import org.koin.dsl.navigation3.navigation
 
 actual fun Module.platformSettingsNavigation() {
-    navigation<JvmSettingsScreen> { JvmSettingsScreen() }
+    settingsNavigation<JvmSettingsScreen> { JvmSettingsScreen() }
 }

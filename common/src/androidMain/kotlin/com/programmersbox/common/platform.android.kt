42d565f91d0f045ff97ea90fc9a2bba340ada282
jacobrein <jacob.rein@gabb.com>
2026-04-08 14:00:05 -0600

    Chore: Update Room and Paging dependencies and cleanup Android Room builder
    This commit updates the versions for AndroidX Room and Paging libraries and removes an unnecessary parameter in the Room database builder for Android.
    
    - **`platform.android.kt`**:
        - Removed the redundant `context` parameter from `Room.databaseBuilder` as the `name` parameter (absolute path) is sufficient for the builder configuration.
    
    - **`libs.versions.toml`**:
        - Updated `paging-version` from `3.5.0-alpha01` to `3.5.0-beta01`.
        - Updated `androidx-room` from `3.0.0-alpha02` to `3.0.0-alpha03`.

--- common/src/androidMain/kotlin/com/programmersbox/common/platform.android.kt
+++ common/src/androidMain/kotlin/com/programmersbox/common/platform.android.kt
@@ -115,7 +115,6 @@ fun getDatabaseBuilder(ctx: Context): RoomDatabase.Builder<AppDatabase> {
     val appContext = ctx.applicationContext
     val dbFile = appContext.getDatabasePath("my_room.db")
     return Room.databaseBuilder<AppDatabase>(
-        context = appContext,
         name = dbFile.absolutePath
     )
 }
